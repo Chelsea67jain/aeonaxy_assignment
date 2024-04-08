c34e742fdfc69017bad67a6a8d6df754a76ea35b
Chelsea Jain <chalsijain38@gmail.com>
2024-04-08 23:40:24 +0530

    Updated..

--- src/Components/FirstPage.js
+++ src/Components/FirstPage.js
@@ -1,3 +1,10 @@
+import Student from '../images/student.png';
+import Professional from '../images/professional.png';
+import Parent from "../images/parent.png";
+import Learner from "../images/lifelong learner.png";
+import Teacher from "../images/teacher.png";
+import Other from "../images/other.png";
+
 
 
 export default function FirstPage(){
@@ -13,12 +20,36 @@ export default function FirstPage(){
             {" "}
             This will help us personalize your experience
           </p>
-          <div className="border-box h-16 w-96 border-2 mt-4 rounded-md"></div>
-          <div className="border-box h-16 w-96 border-2 mt-2 rounded-md"></div>
-          <div className="border-box h-16 w-96 border-2 mt-2 rounded-md"></div>
-          <div className="border-box h-16 w-96 border-2 mt-2 rounded-md"></div>
-          <div className="border-box h-16 w-96 border-2 mt-2 rounded-md"></div>
-          <div className="border-box h-16 w-96 border-2 mt-2 rounded-md"></div>
+          <div className="p-5 flex items-center border-box h-16 w-96 border-2 mt-4 rounded-md">
+            <img src={`${Student}`} className="h-12 w-12" />
+            <b>Student</b> &nbsp;
+            <span className="text-slate-400"> or soon to be enrolled</span>
+          </div>
+          <div className="p-5 flex items-center border-box h-16 w-96 border-2 mt-2 rounded-md">
+            <img src={`${Professional}`} className="h-12 w-12" />
+            <b>Professional</b> &nbsp;{" "}
+            <span className="text-slate-400">pursuing a career</span>
+          </div>
+          <div className="p-5 flex items-center border-box h-16 w-96 border-2 mt-2 rounded-md">
+            <img src={`${Parent}`} className="h-12 w-12" />
+            &nbsp; <b>Parent</b> &nbsp;{" "}
+            <span className="text-slate-400">of a school aged child</span>
+          </div>
+          <div className="p-5 flex items-center border-box h-16 w-96 border-2 mt-2 rounded-md">
+            <img src={`${Learner}`} className="h-12 w-12" />
+            &nbsp; <b>Lifelong learner</b>
+          </div>
+
+          <div className="p-5 flex items-center border-box h-16 w-96 border-2 mt-2 rounded-md">
+            <img src={`${Teacher}`} className="h-12 w-12" />
+            &nbsp;<b>Teacher</b>
+          </div>
+
+          <div className="p-5 flex items-center border-box h-16 w-96 border-2 mt-2 rounded-md">
+            <img src={`${Other}`} className="h-12 w-12" />
+            &nbsp;<b>Other</b>
+          </div>
+
           <button className="h-12 w-36 bg-gray-500 text-white mt-4 hover:bg-gray-400 active:bg-gray-400 focus:outline-none rounded-md font-semibold tracking-normal">
             Continue
           </button>
